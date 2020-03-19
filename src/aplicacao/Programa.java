fa696df04046ba39823c55b53099a9f906890294
Erick Almeida <erickprofissional@gmail.com>
2020-03-19 19:02:23 -0300

    Entidades Criadas

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,3 +1,11 @@
+/*
+Adona de um pensionato possui dez quartos para alugar para estudantes, sendo esses quartos identificados pelos números 0a9.
+Fazer um programa que inicie com todos os dez quartos vazios, e depois leia uma quantidade N representando o 
+número de estudantes que vão alugar quartos (N pode ser de 1 a 10). Em seguida, registre o aluguel dos N estudantes.
+Para cada registro de aluguel, informar o nome e email do estudante, bem como qual dos quartos ele escolheu (de 0 a 9).
+Suponha quesejaescolhidoumquartovago.Aofinal,seuprogramadeveimprimir um relatório de todas ocupações do pensionato, 
+por ordem de quarto, conformeexemplo.
+*/
 package aplicacao;
 
 import java.util.Locale;
@@ -11,6 +19,18 @@ public class Programa {
 		Locale.setDefault(Locale.US);
 		
 		Scanner sc = new Scanner(System.in);
+		
+		Quartos[] vetor = new Quartos[10];
+		
+		System.out.print("Digite a quantidade de quartos: ");
+		int n = sc.nextInt();
+		
+		for(int i = 0;i < n; i++) 
+		{
+			
+			
+		}
+		
 		
 		
 		
