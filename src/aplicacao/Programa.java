44c02ff44a268908de50ca7a13d5fed2197f8532
Erick Almeida <erickprofissional@gmail.com>
2020-03-20 14:36:01 -0300

    Projeto finalizado

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,5 +1,6 @@
 /*
 Adona de um pensionato possui dez quartos para alugar para estudantes, sendo esses quartos identificados pelos números 0a9.
+
 Fazer um programa que inicie com todos os dez quartos vazios, e depois leia uma quantidade N representando o 
 número de estudantes que vão alugar quartos (N pode ser de 1 a 10). Em seguida, registre o aluguel dos N estudantes.
 Para cada registro de aluguel, informar o nome e email do estudante, bem como qual dos quartos ele escolheu (de 0 a 9).
@@ -11,6 +12,8 @@ package aplicacao;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entidades.Quarto;
+
 public class Programa {
 
 	public static void main(String[] args) 
@@ -20,20 +23,36 @@ public class Programa {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		Quartos[] vetor = new Quartos[10];
+		Quarto[] vetor = new Quarto[10];
 		
 		System.out.print("Digite a quantidade de quartos: ");
 		int n = sc.nextInt();
 		
 		for(int i = 0;i < n; i++) 
 		{
+			sc.nextLine();
+			System.out.printf ("Cadastro# "+ i+"\n");
 			
+			System.out.print("Nome:");
+			String nome = sc.nextLine();
 			
+			System.out.print("Email: ");
+			String email = sc.nextLine();
+			
+			System.out.print("Numero do Quarto: ");
+			int numeroQuarto = sc.nextInt();
+					
+			vetor[numeroQuarto]	= new Quarto(nome, email, numeroQuarto);
 		}
-		
-		
-		
-		
+
+		System.out.println("Registro dos Quartos: ");
+		for(int i = 0; i < vetor.length; i++)
+		{
+			if(vetor[i] != null) 
+			{
+				System.out.printf("# "+vetor[i].getQuarto()+": " + vetor[i].getNome()+", "+vetor[i].getEmail()+"\n");
+			}
+		}		
 		sc.close();
 		
 	}
